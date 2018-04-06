431a3116882deafdd708d62474279746db6f8f2c
Howard Grant <geovaniluigid@gmail.com>
2018-04-06 17:31:53 -0400

    Phoneword

---
+++ MainActivity.cs
@@ -0,0 +1,71 @@
+﻿/*
+ *
+ * @author sgtcadet 
+ */
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Content;
+using Android.Net;
+
+namespace Phoneword
+{
+    [Activity(Label = "Phone Word", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        //list that will be filled with numbers
+        static readonly List<string> phoneNumbers = new List<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //getting controls from layout
+            EditText etPhoneNumberText = FindViewById<EditText>(Resource.Id.phoneNumberText);
+            Button btnTranslateButton = FindViewById<Button>(Resource.Id.translateButton);
+            TextView tvTranslatedPhoneWord = FindViewById<TextView>(Resource.Id.translatedPhoneWord);
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
+            Button buttonPlaceCall = FindViewById<Button>(Resource.Id.btnPlaceCall);
+
+            btnTranslateButton.Click += (sender, e) =>
+            {
+                string translatedNumber = Core.PhoneTranslator.ToNumber(etPhoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    tvTranslatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    tvTranslatedPhoneWord.Text = translatedNumber;
+                    phoneNumbers.Add(translatedNumber);
+                    buttonPlaceCall.Enabled = true;
+                    translationHistoryButton.Enabled = true;
+                }
+            };
+
+            translationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
+
+            //Place call button function
+            buttonPlaceCall.Click += (sender, e) =>
+            {
+                //TODO: (1) display dialog box
+                Intent intent = new Intent(Intent.ActionDial);
+                intent.SetData(Uri.Parse("tel:" + tvTranslatedPhoneWord.Text));
+                StartActivity(intent);
+                //TODO: (2) ask user if they want to place the call, if yes place call, if no ? close dialog
+            };
+        }
+    }
+}
+
---
+++ PhoneTranslator.cs
@@ -0,0 +1,69 @@
+﻿/*
+ *
+ * @author sgtcadet 
+ */
+using System.Text;
+using System;
+using System.Linq;//added because of error presented at line 21
+
+namespace Core
+{
+
+    public static class PhoneTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+
+                return "";
+            else
+                raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach ( var c in raw)
+            {
+                if (" -1234567890".Contains(c))
+                {
+                    newNumber.Append(c);
+                }
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                }
+            }
+            return newNumber.ToString();
+        }
+
+        static int? TranslateToNumber(char c)
+        {
+            if ("ABC".Contains(c))
+                return 2;
+            else if ("DEF".Contains(c))
+                return 3;
+            else if ("GHI".Contains(c))
+                return 4;
+            else if ("JKL".Contains(c))
+                return 5;
+            else if ("MNO".Contains(c))
+                return 6;
+            else if ("PQRS".Contains(c))
+                return 7;
+            else if ("TUV".Contains(c))
+                return 8;
+            else if ("wxyz".Contains(c))
+                return 9;
+            return null;
+        }
+
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+        
+    }
+
+
+}
